897a067151f0b799f6c6075e54bf20fd8e3e1841
Mike Sassak <msassak@gmail.com>
2016-02-20 22:03:55 -0800

    Start parsing config file

--- kallice-master/main.go
+++ kallice-master/main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"flag"
+	"github.com/BurntSushi/toml"
 	"log"
 )
 
+type MasterConfig struct {
+	LogRoot    string `toml:"log_root"`
+	LogLevel   string `toml:"log_level"`
+	PidRoot    string `toml:"pid_root"`
+	Include    string
+	ListenPort int `toml:"listen_port"`
+	Producers  map[string]ProducerConfig
+	Runners    map[string]RunnerConfig
+	Jobs       map[string]JobConfig
+}
+
+type ProducerConfig struct {
+	Type     string
+	Interval int
+}
+
+type RunnerConfig struct {
+	Type       string
+	ReturnWait int `toml:"return_wait"`
+}
+
+type JobConfig struct {
+	Producer string
+	Runner   string
+	Command  string
+	Args     []string
+}
+
 func main() {
-	log.Printf("kallice-master doesn't do anything yet")
+	flag.Parse()
+	var configPath = flag.Arg(0)
+
+	if len(configPath) == 0 {
+		log.Fatal("Need path to config file.")
+	}
+
+	log.Printf("configPath is %s", configPath)
+
+	var conf MasterConfig
+	_, err := toml.DecodeFile(configPath, &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("kallice-master read config data! %v", conf)
 }
